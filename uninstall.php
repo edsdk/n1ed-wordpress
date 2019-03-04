dfaf916855c6aa57603e847ad895a5da446b2290
ak <ak@ak-ASUS>
2019-03-04 13:35:34 +0200

    - Removed POST query on first activation. # Fixed bug with multiple plugin headers.

--- uninstall.php
+++ uninstall.php
@@ -1,18 +1,5 @@
 <?php
 /**
- * Plugin Name: N1ED
- * Plugin URI:  https://n1ed.com
- * Description: #1 editor for your content. Create and edit in WYSIWYG style responsive content based on Bootstrap framework
- * Version:     1.1.0
- * Author:      EdSDK
- * Author URI:  https://n1ed.com
- * License:     GPL2
- * License URI: https://www.gnu.org/licenses/gpl-2.0.html
- * Text Domain: n1ed
- * Domain Path: /languages
- */
-
-/**
  * N1ED — #1 editor for your content. Create and edit in WYSIWYG style responsive content based on Bootstrap framework.
  * @encoding     UTF-8
  * @version      1.1.0
@@ -30,10 +17,8 @@ if ( ( ! defined( 'ABSPATH' ) ) && ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) ) {
 
 /** Remove site options. */
 delete_option( 'n1ed_key' );
-delete_option( 'n1ed_first_time_installed' );
 delete_option( 'n1ed_dev_prefix' );
 
 /** Remove site options in Multisite. */
 delete_site_option( 'n1ed_key' );
-delete_site_option( 'n1ed_first_time_installed' );
 delete_site_option( 'n1ed_dev_prefix' );
--- uploader/lib/action/ActionError.php
+++ uploader/lib/action/ActionError.php
@@ -1,18 +1,5 @@
 <?php
 /**
- * Plugin Name: N1ED
- * Plugin URI:  https://n1ed.com
- * Description: #1 editor for your content. Create and edit in WYSIWYG style responsive content based on Bootstrap framework
- * Version:     1.1.0
- * Author:      EdSDK
- * Author URI:  https://n1ed.com
- * License:     GPL2
- * License URI: https://www.gnu.org/licenses/gpl-2.0.html
- * Text Domain: n1ed
- * Domain Path: /languages
- */
-
-/**
  * N1ED — #1 editor for your content. Create and edit in WYSIWYG style responsive content based on Bootstrap framework.
  * @encoding     UTF-8
  * @version      1.1.0
