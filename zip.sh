9218705783e1dc6e51bed524064c277d52653f51
avgustine <ilia@kandaurov.su>
2021-01-11 10:32:03 +0300

    Packing script archiving only necessary files

--- zip.sh
+++ zip.sh
@@ -6,4 +6,4 @@ then
     exit
 fi
 rm zip/n1ed-wordpress.zip
-zip -r zip/n1ed-wordpress.zip . -x './zip.sh' './zip/*' './.git/*' './flmngr-cache/[!.]*'
+zip -r zip/n1ed-wordpress.zip . -i './flmngr-cache/.gitignore' '*.php' './js/*' './tmp/' './vendor/*'
